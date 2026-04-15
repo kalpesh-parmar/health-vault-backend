f89bb3a27a36229a3d5a356aa17391e7423eb0f3
tulsi <tulsihirpara28@gmail.com>
2026-04-15 15:45:34 +0530

    [TH] add drizzle.config.js file

---
+++ drizzle.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+console.log("DB URL:", process.env.DATABASE_URL);
+const { defineConfig } = require("drizzle-kit");
+
+console.log("CONFIG LOADED ✅");
+
+module.exports = defineConfig({
+  schema: "./src/models/index.js",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
