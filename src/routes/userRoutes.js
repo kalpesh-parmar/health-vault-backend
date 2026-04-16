ace2d03d23e2f7576970e1b21a942aec20775e47
tulsi <tulsihirpara28@gmail.com>
2026-04-16 16:40:28 +0530

    [TH] marge with database_setup

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/login", userController.loginUser);
-router.post("/add",userController.createUser);
-router.get("/:id",userController.getUserById);
-router.get("/list",userController.getUserList);
-router.put("/:id",userController.updateUser);
-router.delete("/:id",userController.deleteUser);
-
+router.post("/add", userController.createUser);
+router.get("/:id", userController.getUserById);
+router.get("/list", userController.getUserList);
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const bcrypt = require("bcrypt");
 const { userSchema } = require("../validation/zodValidation");
-const {session:sessionTable}=require("../models/session")
-const {user}=require("../models/User")
-const { db } = require("../config/db"); 
+const { session: sessionTable } = require("../models/session");
+const { user } = require("../models/User");
+const { db } = require("../config/db");
 const userRepository = require("../repositories/userRepository");
 const messageConstant = require("../constant/messageConstant");
 const jwt = require("jsonwebtoken");
@@ -23,17 +23,21 @@ class userService {
   }
   //Login User
   async loginUser(data) {
-      const { email, password } = data;
-     const userData = await userRepository.loginUser(email);
-     if (!userData) {
+    const { email, password } = data;
+    const userData = await userRepository.loginUser(email);
+    if (!userData) {
       throw new InvalidRequestException(messageConstant.INVALID_REQUEST);
     }
 
     //Sessiondata
-   const session = await userRepository.createSession({userId: userData.id});
-    const tempToken = jwt.sign({ sessionId: session.id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
+    const session = await userRepository.createSession({ userId: userData.id });
+    const tempToken = jwt.sign(
+      { sessionId: session.id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
     return {
       tempToken,
     };
