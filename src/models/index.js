2e7f1f96976c4d95b5dde83dc0691f5aabb8b40a
shraddha <shraddhac734@gmail.com>
2026-05-06 15:38:35 +0530

    feat(ocr): implement structured OCR extraction

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ const { document } = require("./document");
 const { notification } = require("./notification");
 const { patient } = require("./patient");
 const { session } = require("./session");
+// const {}=require("./medication");
 
 module.exports = {
   document,
