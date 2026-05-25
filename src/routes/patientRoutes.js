20d57fe31dee75c25723e0478dc627687c2f0557
shraddha <shraddhac734@gmail.com>
2026-05-25 14:33:36 +0530

    fix(commit): resolve commits

--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const patientController = require("../controllers/patientController");
 const { verifyToken } = require("../middlewares/authMiddleware");
-const { upload } = require("../middlewares/upload");
 
 const router = express.Router();
 
-router.post("/add", upload.single("profilePicture"), patientController.createPatient);
+router.post("/add", patientController.createPatient);
 router.get("/list", verifyToken, patientController.getPatientList);
 router.get("/profile", verifyToken, patientController.getPatientProfile);
 router.get("/:id", verifyToken, patientController.getPatientById);
-router.put("/:id", verifyToken, upload.single("profilePicture"), patientController.updatePatient);
+router.put("/:id", verifyToken, patientController.updatePatient);
 router.delete("/soft-delete/:id", verifyToken, patientController.deletePatient);
 router.delete("/hard-delete/:id", verifyToken, patientController.permanentDeletePatient);
-router.delete("/profile-picture-delete", verifyToken, patientController.profilePicDelete);
 
 module.exports = router;
